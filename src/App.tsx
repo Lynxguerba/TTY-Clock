ab9d6a1761c9c95e7c97c1fbc790f6e2cc3921a2
GU15 <119710589+Lynxguerba@users.noreply.github.com>
2025-10-05 19:16:55 +0800

    Added new image and updated code section

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,13 @@ export default function App() {
     return () => clearInterval(id);
   }, []);
 
-  const hours = pad(now.getHours());
+  let hours = now.getHours();
   const minutes = pad(now.getMinutes());
   const seconds = pad(now.getSeconds());
+  const ampm = hours >= 12 ? "PM" : "AM";
+
+  // convert to 12-hour format
+  hours = hours % 12 || 12;
 
   const dateString = now.toLocaleDateString(undefined, {
     weekday: "long",
@@ -24,14 +28,12 @@ export default function App() {
   return (
     <div className="tty-container">
       <div className="tty-time">
-        {hours}:{minutes}:{seconds}
+        {pad(hours)}:{minutes}:{seconds} <span className="ampm">{ampm}</span>
       </div>
       <div className="tty-date">{dateString}</div>
 
-       {/* footer */}
-      <footer className="tty-footer">
-        Created by Dinno Guerba
-      </footer>
+      {/* footer */}
+      <footer className="tty-footer">Created by Dinno Guerba</footer>
     </div>
   );
 }
